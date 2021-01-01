98162b21584f36287006bdd4985b3ba3804c3ce9
Amna <amnae94@gmail.com>
2021-01-01 19:46:59 +0000

    Remove ignored files

--- src/Config/Firebase/db_config.js
+++
@@ -1,9 +0,0 @@
-export const DB_CONFIG = {
-    apiKey: "AIzaSyAOSxUnrSgzPLRqA8MlMEtN0prOpDaBWog",
-    authDomain: "practice-naming.firebaseapp.com",
-    projectId: "practice-naming",
-    storageBucket: "practice-naming.appspot.com",
-    messagingSenderId: "774547797470",
-    appId: "1:774547797470:web:0b31a665085c7eb342994f",
-    measurementId: "G-T3NX8H95V7"
-  }
