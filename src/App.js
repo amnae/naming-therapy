58d37db2e770823ab4ed071a90a894f5eaa8ba74
Amna <amnae94@gmail.com>
2021-01-01 19:35:00 +0000

    completed yputube tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Card from './Card/Card';
+import DrawButton from "./DrawButton/DrawButton";
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { DB_CONFIG } from './Config/Firebase/db_config';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    if (!firebase.apps.length) {
+       this.app = firebase.initializeApp(DB_CONFIG);
+    } else {
+       this.app = firebase.app(); // if already initialized, use that one
+    }
+    
+    this.database = this.app.database().ref().child('words');
+
+    this.updateCard = this.updateCard.bind(this);
+    
+    this.state = {
+        cards: [],
+        currentCard: {}
+    }
+  }
+  
+  componentWillMount(){
+    const currentCards = this.state.cards;
+    
+    this.database.on('child_added', snapshot => {
+        currentCards.push({
+            id: snapshot.key,
+            arabic: snapshot.val().arabic,
+            english: snapshot.val().english
+            
+        })
+        
+        this.setState({
+          cards: currentCards,
+          currentCard: this.getRandomCard(currentCards)
+        })
+    });
+    
+  }
+  
+  getRandomCard(currentCards){
+    var card = currentCards[Math.floor(Math.random() * currentCards.length)];
+    console.log(currentCards.length);
+    console.log(card);
+    return card;
+  }
+  
+  updateCard(){
+    const currentCards = this.state.cards;
+    this.setState({
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="cardRow">
+          <Card eng={this.state.currentCard.english} 
+                ara={this.state.currentCard.arabic} 
+                />
+        </div>
+        <div className="buttonRow">
+          <DrawButton drawCard={this.updateCard}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
